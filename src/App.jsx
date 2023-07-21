e6303167493c271eec725879c9d1b0468b1f8645
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-07-21 02:30:36 -0300

    se guarda cambios en pagina detalle producto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import DetalleProducto from "./components/views/DetalleProducto";
 
 function App() {
   return (
     <>
-      <DetalleProducto></DetalleProducto>
     </>
   );
 }
