cc895ae3a759976105dacaf753c013162c167f0c
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-22 20:37:02 -0300

    agregado css pagina principal inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Inicio from "./components/views/Inicio";
 
 function App() {
   return (
     <>
-    <h1>Hello World!</h1>
+    <Inicio></Inicio>
     </>
   );
 }
