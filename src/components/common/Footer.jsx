0cf8d468a9348ef405cf6d1e580909906dc86f62
carabajal-facundo <facarabajal@hotmail.com>
2023-07-24 12:17:25 -0300

    Se agrego un maquetado basico del footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Facebook, GeoAltFill, Instagram, TelephoneFill, Twitter } from 'react-bootstrap-icons';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Footer</h2>
-        </div>
+        <>
+        <footer className='bg-dark text-light text-center'>
+            <article className='row w-100'>
+                <section className='col-md-6'>
+                    <h3>Nuestas redes</h3>
+                    <p>Instagram <Instagram/></p>
+                    <p>Facebook <Facebook/></p>
+                    <p>Twitter <Twitter/></p>
+                </section>
+                <section className='col-md-6'>
+                    <h3>Nos ubicamos en</h3>
+                    <p><GeoAltFill/> AV. xxxx al xxx</p>
+                    <h3>Telefono de contacto</h3>
+                    <p><TelephoneFill/> xxxx-xxxxxx</p>
+                </section>
+            </article>
+        </footer>
+        </>
     );
 };
 
