a0dad7f0a373242f7cec6f25422a8f88c94b0e22
carabajal-facundo <facarabajal@hotmail.com>
2023-07-24 15:29:29 -0300

    Correccion de los colores a utilizar establecidos en el mockup

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Facebook, GeoAltFill, Instagram, TelephoneFill, Twitter } from 'react-bootstrap-icons';
+import "../../css/Inicio.css"
 
 const Footer = () => {
     return (
         <>
-        <footer className='bg-dark text-light text-center'>
+        <footer className='bg-footer text-light text-center'>
             <article className='row w-100'>
                 <section className='col-md-6'>
                     <h3>Nuestas redes</h3>
-                    <p>Instagram <Instagram/></p>
-                    <p>Facebook <Facebook/></p>
-                    <p>Twitter <Twitter/></p>
+                    <p>Instagram <Instagram className='text-black fs-4'/></p>
+                    <p>Facebook <Facebook className='text-black fs-4'/></p>
+                    <p>Twitter <Twitter className='text-black fs-4'/></p>
                 </section>
                 <section className='col-md-6'>
                     <h3>Nos ubicamos en</h3>
-                    <p><GeoAltFill/> AV. xxxx al xxx</p>
+                    <p><GeoAltFill className='text-black fs-4'/> AV. xxxx al xxx</p>
                     <h3>Telefono de contacto</h3>
-                    <p><TelephoneFill/> xxxx-xxxxxx</p>
+                    <p><TelephoneFill className='text-black fs-4'/> xxxx-xxxxxx</p>
                 </section>
             </article>
         </footer>
