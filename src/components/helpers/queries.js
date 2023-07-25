ce81eb574b55840f674935455e71ae023d6150bf
carabajal-facundo <facarabajal@hotmail.com>
2023-07-25 16:24:27 -0300

    Se agrego en queries.js las consulta para obtener los servicios de la vase de datos

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,1 +1,11 @@
-const queries = "queries";
+const URL_SERVICIOS = impor.meta.env.VITE_API_SERVICIOS;
+
+export const obtenerServicios = async ()=>{
+    try {
+        const respuesta = await fetch(URL_SERVICIOS);
+        const listadoServicios = await respuesta.json();
+        return listadoServicios;
+    } catch (error) {
+        console.log(error);
+    }
+};
--- src/components/views/components_inicio/Servicios.jsx
+++ src/components/views/components_inicio/Servicios.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardServicio from './CardServicio';
 import '../../../css/servicios.css'
+import { obtenerServicios } from '../../helpers/queries';
 
 const Servicios = () => {
+    const [servicios, setServicios] = useState([]);
+    useEffect(()=>{
+        obtenerServicios().then(res=>{
+            setServicios(res)
+        })
+    },[]);
+    
     return (
         <>
         <section className='container text-center bg-container-servicios'>
