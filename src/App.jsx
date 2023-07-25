fa08d0b2206989fe039b6e6c8d73747def1862bc
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-07-25 09:41:56 -0300

    se empieza a maquetar formulario de envio de testimonios

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Inicio from "./components/views/Inicio";
 
 function App() {
   return (
     <>
+    <Inicio></Inicio>
     </>
   );
 }
--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import { EnvelopeFill } from "react-bootstrap-icons";
 
 const Inicio = () => {
-    return (
-        <div>
-            Inicio
-        </div>
-    );
+  return (
+    <section className="mainSection">
+      {/* Formulario Testimonios */}
+      <Container className=" d-flex justify-content-center">
+        <Col md={8}>
+          <div className="border px-3 px-md-4 px-lg-5 mx-md-2 mx-lg-5 py-3 mt-5 rounded-3 shadow">
+            <Row>
+              <Col xs={12}>
+                <div className="d-flex flex-row justify-content-between align-items-center">
+                  <h5>Envíanos tu comentario</h5>
+                  <EnvelopeFill></EnvelopeFill>
+                </div>
+              </Col>
+              <Col className="mt-3">
+                <Form>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control type="email" />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Puntuación</Form.Label>
+                    <Form.Control type="text" />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  >
+                    <Form.Label>Comentario</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                  </Form.Group>
+                </Form>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Container>
+    </section>
+  );
 };
 
 export default Inicio;
