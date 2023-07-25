75fde302e5f52ca097b54cdde56830b80c92e3b4
Silvio Ruiz <silviojrck@gmail.com>
2023-07-25 17:06:50 -0300

    Agrega primera version de pagina error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Error404 from "./components/views/Error404";
 
 function App() {
   return (
     <>
     <h1>Hello World!</h1>
     <h2>Se agrega Rama Dev</h2>
+    <Error404/>
     </>
   );
 }
--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Button, Col, Container, Row } from "react-bootstrap";
+import error404Img from "../../assets/error404.jpg";
+import "./error404.css";
 
 const Error404 = () => {
     return (
-        <div>
-            Error404
-        </div>
+        <Container className="error-404 bg-light rounded my-5">
+            <Row className="my-5 justify-content-center align-items-center p-5 bg-light">
+                <Col sm={6} className="text-center">
+                    <h1 className="error-title mb-4 text-danger bold">Error 404</h1>
+                    <h2>Nuestros cachorros se llevaron esta página. Estamos trabajando en traerla de vuelta pronto.</h2>
+                    <div>
+                        <Button to="/" className="btn btn-danger my-3">Volver al inicio</Button>
+                    </div>
+                </Col>
+                <Col sm={6}>
+                    <img className="img-fluid" src={error404Img} alt="" />
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
