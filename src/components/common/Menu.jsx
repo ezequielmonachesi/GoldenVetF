9f2e95bfae2f0626d3b9c663893346391715c15a
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-25 22:06:13 -0300

    se agrega el logo al proyecto mas una media querie

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,12 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from '../../assets/logo.png'
 
 const Menu = () => {
   return (
-    <Navbar expand="lg" className="bg-navBar">
+    <Navbar expand="lg" className="bg-navBar py-0">
       <Container>
-        <Navbar.Brand href="#home">GoldenVet</Navbar.Brand>
+        <Navbar.Brand href="#home" className="brandNavBar">
+        <img src={logo} alt="" height="55px" width="75px" className="logo"/>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navBarGoldenVet" />
         <Navbar.Collapse id="navBarGoldenVet">
           <Nav className="ms-auto">
