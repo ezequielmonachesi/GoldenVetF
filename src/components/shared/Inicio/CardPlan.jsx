93ee9993a5332a2673fff2b405001946f992a50b
carabajal-facundo <facarabajal@hotmail.com>
2023-07-26 18:34:42 -0300

    Se agrego los planes mostrando una card de la cual surge una ventana modal con mas informacion sobre el mismo

---
+++ src/components/shared/Inicio/CardPlan.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Card, Col, Button, Modal } from 'react-bootstrap';
+
+const CardPlan = ({nombrePlan, rangoEdad, descripcion, serviciosPlan, imagenes}) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    const listado = serviciosPlan.map(servicio=><li>{servicio}</li>);
+    
+    return (
+        <>
+        <Col md={4} ld={3} className="mb-3">
+            <Card className='bg-card-servicio'>
+                <Card.Img src={imagenes[1]} placeholder={nombrePlan}/>
+                <Card.Body>
+                    <Card.Title>{nombrePlan}</Card.Title>
+                    <p>rango de edad: {rangoEdad}</p>
+                    <button className='bg-boton-servicio btn' onClick={handleShow}>Ver mas</button>
+                </Card.Body>
+            </Card>
+        </Col>
+
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{nombrePlan}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <article>
+                    <img src={imagenes[1]} alt={nombrePlan} className='img-fluid'/>
+                </article>
+                <article>
+                    {descripcion}
+                </article>
+                <article>
+                    <h3>Servicios que les ofrecemos</h3>
+                    <ul>
+                        {listado}
+                    </ul>
+                </article>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={handleClose}>
+                    Solicitar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
+    );
+};
+
+export default CardPlan;
--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Planes from '../shared/Inicio/Planes';
 
 const Inicio = () => {
     return (
         <section className='mainSection'>
-          
+            <Planes/>
         </section>
     );
 };
