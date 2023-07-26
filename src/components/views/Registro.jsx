46eb64d09d8f2b32fcff95a4a3c9fbfb2f58ccbb
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-26 17:32:38 -0300

    se agregan las validaciones al formulario de registro

--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -1,35 +1,91 @@
 import { Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Registro = () => {
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="mt-5 mainSection">
       <div className="row justify-content-center">
         <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-          <Form className="form-registro mx-auto p-2 border shadow p-3 mb-5 bg-body-tertiary rounded">
-          <h3 className="text-center registro">Registro</h3>
+          <Form
+            className="form-registro mx-auto p-2 border shadow p-3 mb-5 bg-body-tertiary rounded"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <h3 className="text-center registro">Registro</h3>
             <Form.Group className="mb-2">
               <Form.Control
                 type="text"
                 placeholder="Ingrese un nombre de usuario"
+                {...register("nombreUsuario", {
+                  minLength: {
+                    value: 2,
+                    message: "La cantidad mínima de carateres es de 2 digitos",
+                  },
+                  maxLength: {
+                    value: 16,
+                    message:
+                      "La cantidad maximo de caracteres es de 16 digitos",
+                  },
+                  required: "El nombre de usuario es requerido",
+                })}
               />
+              <Form.Text className="text-danger">
+                {errors.nombreUsuario?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-2">
-              <Form.Control placeholder="Ingrese un email" />
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un email"
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message:
+                      "El email debe tener un formato valido (mail@dominio.com)",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-2">
-              <Form.Control type="password" placeholder="Ingrese un password" />
+              <Form.Control
+                type="password"
+                placeholder="Ingrese un password"
+                {...register("password", {
+                  required: "La contraseña es obligatoria",
+                  pattern: {
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]{8,16}$/,
+                    message:
+                      "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un carácter especial y un número. Además, la longitud máxima es de 16 caracteres.",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <div className="row w-75 d-flex justify-content-center text-center ms-5 ps-lg-5">
-              <button
-                className="btn-registro mb-2 rounded"
-                type="submit"
-              >
+              <button className="btn-registro mb-2 rounded" type="submit">
                 Registrar
               </button>
             </div>
           </Form>
           <div className="d-flex justify-content-end contenedor-inicia-sesion">
-            <p>¿Ya tienes cuenta?  <span className="inicia-sesion">Inicia sesión</span></p>
+            <p>
+              ¿Ya tienes cuenta?{" "}
+              <span className="inicia-sesion">Inicia sesión</span>
+            </p>
           </div>
         </div>
       </div>
