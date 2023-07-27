02e9a751ff5dbf0d7160aa6aba3c00f01bdf2f44
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-27 20:13:06 -0300

    creando la funcion para obtener los productos de la BD

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,6 +1,8 @@
 const queries = "queries";
 const URL_PRODUCTOS = import.meta.env.VITE_API_PRODUCTOS
 
-export const obtenerProductos = () =>{
-console.log(URL_PRODUCTOS)
+export const obtenerProductos = async() =>{
+    const respuesta = await fetch(URL_PRODUCTOS)
+    const data = await respuesta.json()
+    return data
 }
