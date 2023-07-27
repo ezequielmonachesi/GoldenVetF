22f9f149e0813c3ffc30e3e7e7ac3bf4d41f1459
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-27 00:56:22 -0300

    agregandole los distintos breakpoints

--- src/components/views/Producto/CardProducto.jsx
+++ src/components/views/Producto/CardProducto.jsx
@@ -1,20 +1,22 @@
-import { Col } from 'react-bootstrap';
+import { Col, Image } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 
 const CardProducto = () => {
     return (
-        <Col  md={4} ld={3} className="mb-3">
-        <Card className='shadow-lg p-3 mb-5 bg-body-tertiary rounded'>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Col  md={6} lg={3}  xs={12} className="mb-1">
+        <Card className='shadow-lg p-3 mb-3 bg-body-tertiary rounded'>
+        <Card.Header className='p-0'>
+        <Card.Img className='img-productos w-100' src='https://nanolog.vtexassets.com/arquivos/ids/165009-800-800?v=637376716541070000&width=800&height=800&aspect=true'></Card.Img>
+        </Card.Header>
         <Card.Body>
           <Card.Title className='text-center'>Card Title</Card.Title>
-          <Card.Text className='limite-texto'>
+          <Card.Text className='limite-texto text-center'>
           Some quick example text to build on the card title and make up the bulk of the card's content.
           </Card.Text>
-          <div className='text-center'>
-          <button className='boton-verMas-producto fw-semibold rounded'>Ver mas</button>
-          </div>
         </Card.Body>
+        <div className='text-center'>
+          <button className='boton-verMas-producto fw-semibold rounded'>Ver mas</button>
+        </div>
       </Card>
         </Col>
         
--- src/components/views/Productos.jsx
+++ src/components/views/Productos.jsx
@@ -5,7 +5,13 @@ import CardProducto from './Producto/CardProducto';
 const Productos = () => {
     return (
        <Container className='mt-3'>
+        <h1 className='text-center texto-productos fw-bold mt-1'>Nuestros Productos</h1>
+        <hr />
         <Row>
+            <CardProducto/>
+            <CardProducto/>
+            <CardProducto/>
+            <CardProducto/>
             <CardProducto/>
             <CardProducto/>
             <CardProducto/>
