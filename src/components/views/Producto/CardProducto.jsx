0101516edf377b5d5f9de6851fb6912ded3bedbd
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-27 00:59:34 -0300

    se quito import innecesarios e idento codigo

--- src/components/views/Producto/CardProducto.jsx
+++ src/components/views/Producto/CardProducto.jsx
@@ -1,26 +1,31 @@
-import { Col, Image } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
+import { Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
 const CardProducto = () => {
-    return (
-        <Col  md={6} lg={3}  xs={12} className="mb-1">
-        <Card className='shadow-lg p-3 mb-3 bg-body-tertiary rounded'>
-        <Card.Header className='p-0'>
-        <Card.Img className='img-productos w-100' src='https://nanolog.vtexassets.com/arquivos/ids/165009-800-800?v=637376716541070000&width=800&height=800&aspect=true'></Card.Img>
+  return (
+    <Col md={6} lg={3} xs={12} className="mb-1">
+      <Card className="shadow-lg p-3 mb-3 bg-body-tertiary rounded">
+        <Card.Header className="p-0">
+          <Card.Img
+            className="img-productos w-100"
+            src="https://nanolog.vtexassets.com/arquivos/ids/165009-800-800?v=637376716541070000&width=800&height=800&aspect=true"
+          ></Card.Img>
         </Card.Header>
         <Card.Body>
-          <Card.Title className='text-center'>Card Title</Card.Title>
-          <Card.Text className='limite-texto text-center'>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
+          <Card.Title className="text-center">Card Title</Card.Title>
+          <Card.Text className="limite-texto text-center">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
           </Card.Text>
         </Card.Body>
-        <div className='text-center'>
-          <button className='boton-verMas-producto fw-semibold rounded'>Ver mas</button>
+        <div className="text-center">
+          <button className="boton-verMas-producto fw-semibold rounded">
+            Ver más
+          </button>
         </div>
       </Card>
-        </Col>
-        
-    );
+    </Col>
+  );
 };
 
 export default CardProducto;
--- src/components/views/Productos.jsx
+++ src/components/views/Productos.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
-import CardProducto from './Producto/CardProducto';
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import CardProducto from "./Producto/CardProducto";
 
 const Productos = () => {
-    return (
-       <Container className='mt-3'>
-        <h1 className='text-center texto-productos fw-bold mt-1'>Nuestros Productos</h1>
-        <hr />
-        <Row>
-            <CardProducto/>
-            <CardProducto/>
-            <CardProducto/>
-            <CardProducto/>
-            <CardProducto/>
-            <CardProducto/>
-            <CardProducto/>
-        </Row>
-       </Container>
-    );
+  return (
+    <Container className="mt-3">
+      <h1 className="text-center texto-productos fw-bold mt-1">
+        Nuestros Productos
+      </h1>
+      <hr />
+      <Row>
+        <CardProducto />
+        <CardProducto />
+        <CardProducto />
+        <CardProducto />
+        <CardProducto />
+        <CardProducto />
+        <CardProducto />
+      </Row>
+    </Container>
+  );
 };
 
 export default Productos;
