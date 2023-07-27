590b0b681f280f25df59d0d75fc2f36d5ec91a43
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-26 23:45:21 -0300

    se captura el servicio en el elemento detalleservicio para mostrarlo dinamicamente segun id

--- src/components/views/servicios/DetalleServicios.jsx
+++ src/components/views/servicios/DetalleServicios.jsx
@@ -2,13 +2,23 @@ import React, { useEffect } from "react";
 import "./DetallesServicios.css";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import { useParams } from "react-router";
-
+import { obtenerUnServicio } from "../../helpers/queries";
+import { useState } from "react";
 const DetalleServicios = () => {
+
+  const [servicio,setServicio]= useState({});
+  const {id} = useParams();
+
+  useEffect(()=>{
+    obtenerUnServicio(id).then((respuesta)=>{
+      setServicio(respuesta)
+    })
+  })
 
   return (
     <Container className="align-items-center">
       <Row className="m-1 grilla justify-content-center">
-        <h1 className="text-center">Peluqueria Canina</h1>
+        <h1 className="text-center">{servicio.nombreServicio}</h1>
         <Col xs={12} md={6} lg={6} className="py-5 row">
           <Col xs={5} sm={6} lg={4} className="d-flex justify-content-end align-items-center">
             <Image className="imagen-subservicio" src="https://static.wixstatic.com/media/9c39c5_76379079b4e843bcbf072725c845e586.jpg/v1/fill/w_552,h_406,al_c,lg_1,q_80,enc_auto/9c39c5_76379079b4e843bcbf072725c845e586.jpg"
