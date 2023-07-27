d427840b62cd8d91dc850986a01a9c2a15bc8ee6
Silvio Ruiz <silviojrck@gmail.com>
2023-07-27 16:30:37 -0300

    Agrega carrousel de imagenes al detalle del plan

--- src/components/views/detallePlan/DetallePlan.jsx
+++ src/components/views/detallePlan/DetallePlan.jsx
@@ -1,6 +1,6 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import "./style.css";
-import imagenPrimerosPasos1 from "../../../assets/plan-primeros-pasos-1.jpg";
+import CarouselPlanImg from './CarouselPlanImg';
 
 const DetallePlan = () => {
     return (
@@ -11,7 +11,7 @@ const DetallePlan = () => {
                 <p className='plan-description'>Este plan está diseñado para atender a las mascotas en sus etapas más jóvenes, desde el nacimiento hasta los 5 años de edad. Sabemos lo importante que es brindarles un cuidado óptimo durante su desarrollo temprano para asegurar una vida saludable y feliz</p>
                 <Row className='my-5'>
                     <Col sm={6}>
-                        <img className='img-fluid mb-5' src={imagenPrimerosPasos1} alt="" />
+                        <CarouselPlanImg />
                     </Col>
                     <Col sm={6}>
                         <h3 className='text-center mb-3'>Este plan cuenta con los siguientes servicios:</h3>
