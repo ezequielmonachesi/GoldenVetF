c10990a3861ae8b66ac68c5f038eb42da6dd7fee
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-26 23:56:03 -0300

    pagina de detalles de servicios dinamica

--- src/components/views/servicios/DetalleServicios.jsx
+++ src/components/views/servicios/DetalleServicios.jsx
@@ -13,80 +13,37 @@ const DetalleServicios = () => {
     obtenerUnServicio(id).then((respuesta)=>{
       setServicio(respuesta)
     })
-  })
+  },[])
 
   return (
     <Container className="align-items-center">
       <Row className="m-1 grilla justify-content-center">
         <h1 className="text-center">{servicio.nombreServicio}</h1>
-        <Col xs={12} md={6} lg={6} className="py-5 row">
-          <Col xs={5} sm={6} lg={4} className="d-flex justify-content-end align-items-center">
-            <Image className="imagen-subservicio" src="https://static.wixstatic.com/media/9c39c5_76379079b4e843bcbf072725c845e586.jpg/v1/fill/w_552,h_406,al_c,lg_1,q_80,enc_auto/9c39c5_76379079b4e843bcbf072725c845e586.jpg"
-            ></Image>
-          </Col>
-          <Col
-            xs={7}
-            sm={6}
-            className="d-flex flex-column align-items-left justify-content-end texto"
-          >
-            <h6 className="fw-bold">CORTE Y BAÑO COMPLETO</h6>
-            <p className="text-black">              
-              Incluye baño con shampoo natural, secado, corte a máquina y/o
-              tijera, peinado, limpieza de oídos y corte de uñas.
-            </p>
-          </Col>
-        </Col>
-        <Col xs={12} md={6} lg={6} className="py-5 row">
-          <Col xs={5} sm={6} lg={4} className="d-flex justify-content-end align-items-center">
-            <Image className="imagen-subservicio" src="https://static.wixstatic.com/media/9c39c5_76379079b4e843bcbf072725c845e586.jpg/v1/fill/w_552,h_406,al_c,lg_1,q_80,enc_auto/9c39c5_76379079b4e843bcbf072725c845e586.jpg"
-            ></Image>
-          </Col>
-          <Col
-            xs={7}
-            sm={6}
-            className="d-flex flex-column align-items-left justify-content-end texto"
-          >
-            <h6 className="fw-bold">CORTE Y BAÑO COMPLETO</h6>
-            <p className="text-black">              
-              Incluye baño con shampoo natural, secado, corte a máquina y/o
-              tijera, peinado, limpieza de oídos y corte de uñas.
-            </p>
-          </Col>
-        </Col>
-        <Col xs={12} md={6} lg={6} className="py-5 row">
-          <Col xs={5} sm={6} lg={4} className="d-flex justify-content-end align-items-center">
-            <Image className="imagen-subservicio" src="https://static.wixstatic.com/media/9c39c5_76379079b4e843bcbf072725c845e586.jpg/v1/fill/w_552,h_406,al_c,lg_1,q_80,enc_auto/9c39c5_76379079b4e843bcbf072725c845e586.jpg"
-            ></Image>
-          </Col>
-          <Col
-            xs={7}
-            sm={6}
-            className="d-flex flex-column align-items-left justify-content-end texto"
-          >
-            <h6 className="fw-bold">CORTE Y BAÑO COMPLETO</h6>
-            <p className="text-black">              
-              Incluye baño con shampoo natural, secado, corte a máquina y/o
-              tijera, peinado, limpieza de oídos y corte de uñas.
-            </p>
-          </Col>
-        </Col>
-        <Col xs={12} md={6} lg={6} className="py-5 row">
-          <Col xs={5} sm={6} lg={4} className="d-flex justify-content-end align-items-center">
-            <Image className="imagen-subservicio" src="https://static.wixstatic.com/media/9c39c5_76379079b4e843bcbf072725c845e586.jpg/v1/fill/w_552,h_406,al_c,lg_1,q_80,enc_auto/9c39c5_76379079b4e843bcbf072725c845e586.jpg"
-            ></Image>
-          </Col>
-          <Col
-            xs={7}
-            sm={6}
-            className="d-flex flex-column align-items-left justify-content-end texto"
-          >
-            <h6 className="fw-bold">CORTE Y BAÑO COMPLETO</h6>
-            <p className="text-black">              
-              Incluye baño con shampoo natural, secado, corte a máquina y/o
-              tijera, peinado, limpieza de oídos y corte de uñas.
-            </p>
-          </Col>
-        </Col>
+    <p>{servicio.descripcion}</p>
+    <h2 className="text-center">Servicios</h2>
+       {servicio.subservicios && servicio.subservicios.map((subservicio,index) =>(
+                <Col key={index} xs={12} md={6} lg={6} className="py-5 row">
+                <Col xs={5} sm={6} lg={4} className="d-flex justify-content-end align-items-center">
+                  <Image className="imagen-subservicio" src={subservicio.imagenSubservicio}
+                  ></Image>
+                </Col>
+                <Col
+                  xs={7}
+                  sm={6}
+                  className="d-flex flex-column align-items-left justify-content-end texto"
+                >
+                  <h6 className="fw-bold">{subservicio.nombreSubservicio}</h6>
+                  <p className="text-black">              
+                    {subservicio.descripcionSubservicio}
+                  </p>
+                </Col>
+              </Col>
+
+       ))
+       }
+
+
+
       </Row>
     </Container>
   );
