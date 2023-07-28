c53b0e4fdf54c817ccdcb26687838ef94dafb2ba
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-07-28 06:42:47 -0300

    terminado de cards de sobre nosotros

--- src/components/views/AcercaDeNosotros.jsx
+++ src/components/views/AcercaDeNosotros.jsx
@@ -1,78 +1,245 @@
 import React from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import "./sobreNosotros.css";
+import imagenPrincipal from "../../assets/sobreNosotros.jpg";
+import { Link } from "react-router-dom";
 
 const AcercaDeNosotros = () => {
   return (
     <>
-      <Container>
+      <Container fluid>
+        {/* paradax */}
         <Row>
-          <Col>
-            <h2>Queremos cuidar tu mascota</h2>
-            <ul className="list-unstyled">
-              <li>Somos Responsables.</li>
-              <li>Buscamos la mejor opción.</li>
-              <li>Productos de calidad.</li>
-            </ul>
-          </Col>
+          <div className="bgimg-1">
+            <div className="caption">
+              <span>
+                <h3 className="display-3">Queremos cuidar tu mascota</h3>
+              </span>
+            </div>
+          </div>
         </Row>
-      </Container>
-      <Container>
-        <div className="">
-          <div className="img-acercaDeNostros border rounded-3 shadow"></div>
-        </div>
-        <Row>
-          <Col>Somos nosotros</Col>
-          <Col>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
-            tempore, harum voluptates nobis quas illum exercitationem deserunt
-            inventore temporibus consectetur, corporis ullam at voluptate
-            eveniet vel cupiditate tempora, aspernatur ex facere cumque velit.
-            Maxime rem dolorem sint, doloremque voluptatum suscipit fuga
-            praesentium enim, error quisquam excepturi quo nulla ex architecto!
-          </Col>
-        </Row>
-        <Row className="mt-3">
+        <Row className="my-5 py-5">
           <p>Nuestra misión</p>
+          <h2>Cumplir tus expectativas</h2>
+          <Col md={4} className="mt-3">
+            <Card className="border shadow rounded-4">
+              <Card.Body>
+                <div className="pb-3">
+                  <i class="bi bi-bookmark-star-fill fs-2 color"></i>
+                </div>
+                <Card.Title>Enfoque Personal</Card.Title>
+                <Card.Text>
+                  Vas a contar con una atención personalizada con nuestro
+                  equipo.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} className="mt-3">
+            <Card className="border shadow rounded-4">
+              <Card.Body>
+                <div className="pb-3">
+                  <i class="bi bi-bookmark-star-fill fs-2 color"></i>
+                </div>
+                <Card.Title>Enfoque Personal</Card.Title>
+                <Card.Text>
+                  Vas a contar con una atención personalizada con nuestro
+                  equipo.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} className="mt-3">
+            <Card className="border shadow rounded-4">
+              <Card.Body>
+                <div className="pb-3">
+                  <i class="bi bi-bookmark-star-fill fs-2 color"></i>
+                </div>
+                <Card.Title>Enfoque Personal</Card.Title>
+                <Card.Text>
+                  Vas a contar con una atención personalizada con nuestro
+                  equipo.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <div className="bgimg-2">
+            <div className="caption">
+              <h2 className="display-2">Confía en nosotros</h2>
+            </div>
+          </div>
+        </Row>
+        <Row className="my-5 py-5 justify-content-start">
+          <p>Nuestro equipo</p>
           <h2>Equipo Profesional</h2>
-          <Col md={4} className="mt-3">
-            <Card className="border shadow rounded-4">
-              <Card.Body>
-                <div className="pb-3">
-                  <i class="bi bi-bookmark-star-fill fs-2 color"></i>
-                </div>
-                <Card.Title>Enfoque Personal</Card.Title>
-                <Card.Text>
-                  Vas a contar con una atención personalizada con nuestro
-                  equipo.
+          <Col md={4} lg={3} className="mt-3">
+            <Card className="border shadow rounded-4 card">
+              <Card.Img
+                variant="top"
+                src="http://127.0.0.1:5500/img/EzeequielAvatarMaker.png"
+                className="card-img-top"
+              />
+              <Card.Body className="card-body d-flex flex-column justify-content-center align-items-center text-center">
+                <Card.Title>Ezequiel Monahcesi</Card.Title>
+                <Card.Text className="mt-2">
+                  <div className="d-flex">
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-linkedin fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-whatsapp fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                  </div>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col md={4} className="mt-3">
-            <Card className="border shadow rounded-4">
-              <Card.Body>
-                <div className="pb-3">
-                  <i class="bi bi-bookmark-star-fill fs-2 color"></i>
-                </div>
-                <Card.Title>Enfoque Personal</Card.Title>
-                <Card.Text>
-                  Vas a contar con una atención personalizada con nuestro
-                  equipo.
+          <Col md={4} lg={3} className="mt-3">
+            <Card className="border shadow rounded-4 card">
+              <Card.Img
+                variant="top"
+                src="http://127.0.0.1:5500/img/EzeequielAvatarMaker.png"
+                className="card-img-top"
+              />
+              <Card.Body className="card-body d-flex flex-column justify-content-center align-items-center  text-center">
+                <Card.Title>Ezequiel Monahcesi</Card.Title>
+                <Card.Text className="mt-2">
+                  <div className="d-flex">
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-linkedin fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-whatsapp fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                  </div>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col md={4} className="mt-3">
-            <Card className="border shadow rounded-4">
-              <Card.Body>
-                <div className="pb-3">
-                  <i class="bi bi-bookmark-star-fill fs-2 color"></i>
-                </div>
-                <Card.Title>Enfoque Personal</Card.Title>
-                <Card.Text>
-                  Vas a contar con una atención personalizada con nuestro
-                  equipo.
+          <Col md={4} lg={3} className="mt-3">
+            <Card className="border shadow rounded-4 card">
+              <Card.Img
+                variant="top"
+                src="http://127.0.0.1:5500/img/EzeequielAvatarMaker.png"
+                className="card-img-top"
+              />
+              <Card.Body className="card-body d-flex flex-column justify-content-center align-items-center text-center">
+                <Card.Title>Ezequiel Monahcesi</Card.Title>
+                <Card.Text className="mt-2">
+                  <div className="d-flex">
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-linkedin fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-whatsapp fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} lg={3} className="mt-3">
+            <Card className="border shadow rounded-4 card">
+              <Card.Img
+                variant="top"
+                src="http://127.0.0.1:5500/img/EzeequielAvatarMaker.png"
+                className="card-img-top"
+              />
+              <Card.Body className="card-body d-flex flex-column justify-content-center align-items-center text-center">
+                <Card.Title>Ezequiel Monahcesi</Card.Title>
+                <Card.Text className="mt-2">
+                  <div className="d-flex">
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-linkedin fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-whatsapp fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                  </div>
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4} lg={3} className="mt-3">
+            <Card className="border shadow rounded-4 card">
+              <Card.Img
+                variant="top"
+                src="http://127.0.0.1:5500/img/EzeequielAvatarMaker.png"
+                className="card-img-top"
+              />
+              <Card.Body className="card-body d-flex flex-column justify-content-center align-items-center text-center">
+                <Card.Title>Ezequiel Monahcesi</Card.Title>
+                <Card.Text className="mt-2">
+                  <div className="d-flex">
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-linkedin fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                    <a
+                      className="text-decoration-none mx-1"
+                      href="https://www.linkedin.com/in/c%C3%A9sar-ezequiel-monachesi-a95a38213/"
+                      target="_blank"
+                    >
+                      <span className="circle">
+                        <i class="bi bi-whatsapp fs-5 iconColor"></i>
+                      </span>
+                    </a>
+                  </div>
                 </Card.Text>
               </Card.Body>
             </Card>
