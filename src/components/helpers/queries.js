c3574447ed4d0032b85d70d52299a04d93bebb8d
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-28 20:43:49 -0300

    se le agrega un bloque try catch a la fucnion obtenerProductos

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -3,9 +3,13 @@ const URL_PRODUCTOS = import.meta.env.VITE_API_PRODUCTOS
 const URL_SERVICIOS = import.meta.env.VITE_API_SERVICIOS;
 
 export const obtenerProductos = async() =>{
+  try {
     const respuesta = await fetch(URL_PRODUCTOS)
     const data = await respuesta.json()
     return data
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export const obtenerServicios = async ()=>{
