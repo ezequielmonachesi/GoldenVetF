a74198d6bfd3bdcf7f2ebff7e89ca0bd54b1b8e5
carabajal-facundo <facarabajal@hotmail.com>
2023-07-27 22:26:13 -0300

    Se cambio algunas de las imagenes para el carrusel

--- src/components/shared/Inicio/Publicidad.jsx
+++ src/components/shared/Inicio/Publicidad.jsx
@@ -10,7 +10,7 @@ const Publicidad = () => {
                 <img 
                     src="https://www.petmania.com.ar/imgs/mcat/mcat-banner-perros-alimento.png?v=8" 
                     alt="Publicidad de alimento para perro" 
-                    className='img-fluid w-100 img-altura'/>
+                    className=' w-100 img-altura'/>
                 <Carousel.Caption>
                 </Carousel.Caption>
             </Carousel.Item>
@@ -18,15 +18,15 @@ const Publicidad = () => {
                 <img 
                     src="https://www.whiskas.com.ar/cdn-cgi/image/format%3Dauto%2Cq%3D90/sites/g/files/fnmzdf2296/files/2022-12/Adult_hero_desktop-ar.png" 
                     alt="Alimento para gatos Whiskas"
-                    className='img-fluid w-100 img-altura' />
+                    className=' w-100 img-altura' />
                 <Carousel.Caption>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    src="https://guialocal-concordia.s3.us-east-2.amazonaws.com/2019/07/4_patitas_peluquerÃ%C2%ADa_canina_11154_banner.png" 
-                    alt="Peluqueria canina a domicilio" 
-                    className='img-fluid w-100 img-altura'/>
+                    src="https://www.wholeearthfarms.com.ar/sites/default/files/styles/frontpage_slider/public/BANNER_HERO-PRODUCTOS.jpeg?itok=H9P5UkP1"
+                    alt="Alimento para perros Whole Earth Farms"
+                    className=' w-100 img-altura'/>
                 <Carousel.Caption>
                 </Carousel.Caption>
             </Carousel.Item>
