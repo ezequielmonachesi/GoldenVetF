a75075fa7c43dfc3634c39034ab9e94399b487e8
Silvio Ruiz <silviojrck@gmail.com>
2023-07-28 21:47:17 -0300

    Actualiza hook que trae la informacion, ahora puede traer de todos los modelos

--- src/components/hooks/useFetchData.jsx
+++ src/components/hooks/useFetchData.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
-import { fetchData } from '../helpers/queriesServicios'; // Reemplaza 'fetchData' por la función real que obtiene los datos del backend
+import { obtenerComentarios, obtenerComentario } from '../helpers/queriesComentarios';
+import { obtenerPacientes, obtenerPaciente } from '../helpers/queriesPacientes';
+import { obtenerProductos, obtenerProducto } from '../helpers/queriesProductos';
+import { obtenerServicios, obtenerServicio } from '../helpers/queriesServicios';
+import { obtenerTurnos, obtenerTurno } from '../helpers/queriesTurnos';
+import { obtenerUsuarios, obtenerUsuario } from '../helpers/queriesUsuarios';
 
 export const useFetchData = (dataType) => {
     const [data, setData] = useState([]);
@@ -10,7 +15,7 @@ export const useFetchData = (dataType) => {
         const getData = async () => {
             setIsLoading(true);
 
-            const response = await fetchData(dataType);
+            const response = await seleccionarDatosATraer(dataType);
             if (!response?.ok) {
                 setError(response?.message);
             } else {
@@ -23,6 +28,37 @@ export const useFetchData = (dataType) => {
 
         getData();
     }, [dataType]);
+
+    async function seleccionarDatosATraer(dataType) {
+        switch (dataType) {
+            case 'comentarios':
+                return obtenerComentarios();
+            case 'comentario':
+                return obtenerComentario(/* Aquí el id del comentario que quieras obtener */);
+            case 'pacientes':
+                return obtenerPacientes();
+            case 'paciente':
+                return obtenerPaciente(/* Aquí el id del paciente que quieras obtener */);
+            case 'productos':
+                return obtenerProductos();
+            case 'producto':
+                return obtenerProducto(/* Aquí el id del producto que quieras obtener */);
+            case 'servicios':
+                return obtenerServicios();
+            case 'servicio':
+                return obtenerServicio(/* Aquí el id del servicio que quieras obtener */);
+            case 'turnos':
+                return obtenerTurnos();
+            case 'turno':
+                return obtenerTurno(/* Aquí el id del turno que quieras obtener */);
+            case 'usuarios':
+                return obtenerUsuarios();
+            case 'usuario':
+                return obtenerUsuario(/* Aquí el id del usuario que quieras obtener */);
+            default:
+                throw new Error(`Tipo de dato no soportado: ${dataType}`);
+        }
+    }
 
     return {
         data,
