383827d44c714bff7e4a948ac7ba53c67291d8a8
Silvio Ruiz <silviojrck@gmail.com>
2023-07-28 21:33:43 -0300

    Agrega todas las queries para productos

--- src/components/helpers/queriesProductos.js
+++ src/components/helpers/queriesProductos.js
@@ -1,11 +1,62 @@
-const URL_SERVICIOS = import.meta.env.VITE_API_SERVICIOS;
+const URL_PRODUCTOS = import.meta.env.VITE_API_PRODUCTOS;
 
-export const obtenerServicios = async ()=>{
+export const obtenerProductos = async ()=>{
     try {
-        const respuesta = await fetch(URL_SERVICIOS);
-        const listadoServicios = await respuesta.json();
-        return listadoServicios;
+        const respuesta = await fetch(URL_PRODUCTOS);
+        const listadoProductos = await respuesta.json();
+        return listadoProductos;
     } catch (error) {
         console.log(error);
     }
 };
+
+export const obtenerProducto = async (id)=>{
+    try{
+        const respuesta = await fetch(URL_PRODUCTOS+'/'+id);
+        const producto = await respuesta.json();
+        return producto;
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export const crearProducto = async (producto)=>{
+    try{
+        const respuesta = await fetch(URL_PRODUCTOS,{
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(producto)
+        });
+        return respuesta;
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export const borrarProducto = async (id)=>{
+    try{
+        const respuesta = await fetch(URL_PRODUCTOS+'/'+id,{
+            method: "DELETE"
+        });
+        return respuesta;
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export const editarProducto = async (producto, id)=>{
+    try{
+        const respuesta = await fetch(URL_PRODUCTOS+'/'+id,{
+            method: "PUT",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(producto)
+        });
+        return respuesta;
+    }catch (error){
+        console.log(error)
+    }
+}
