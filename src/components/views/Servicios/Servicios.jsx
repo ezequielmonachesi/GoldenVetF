d31cc7f398a4892be2f33efa6791334718fbbc0f
Silvio Ruiz <silviojrck@gmail.com>
2023-07-28 22:04:50 -0300

    Cambios para poder cambiar de rama

--- src/components/views/Servicios/Servicios.jsx
+++ src/components/views/Servicios/Servicios.jsx
@@ -22,12 +22,12 @@ const Servicios = () => {
     
     return (
         <>
-        <section className='container text-center bg-container-servicios'>
-            <h2 className='m-5'>Nuestros Servicios</h2>
-            <div className='row justify-content-center'>
-                {listado.length >= 1 ? listado : <h3>Hubo un error al cargar los servicios</h3>}
-            </div>
-        </section>
+            <section className='container text-center bg-container-servicios'>
+                <h2 className='m-5'>Nuestros Servicios</h2>
+                <div className='row justify-content-center'>
+                    {listado.length >= 1 ? listado : <h3>Hubo un error al cargar los servicios</h3>}
+                </div>
+            </section>
         </>
     );
 };
