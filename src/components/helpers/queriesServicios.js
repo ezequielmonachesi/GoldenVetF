fdd4a32405b0e9524cc794297d44126f69dfa60f
Silvio Ruiz <silviojrck@gmail.com>
2023-07-28 21:30:01 -0300

    Agrega todas las queries para servicios

--- src/components/helpers/queriesServicios.js
+++ src/components/helpers/queriesServicios.js
@@ -9,3 +9,54 @@ export const obtenerServicios = async ()=>{
         console.log(error);
     }
 };
+
+export const obtenerServicio = async (id)=>{
+    try{
+        const respuesta = await fetch(URL_SERVICIOS+'/'+id);
+        const servicio = await respuesta.json();
+        return servicio;
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export const crearServicio = async (servicio)=>{
+    try{
+        const respuesta = await fetch(URL_SERVICIOS,{
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(servicio)
+        });
+        return respuesta;
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export const borrarServicio = async (id)=>{
+    try{
+        const respuesta = await fetch(URL_SERVICIOS+'/'+id,{
+            method: "DELETE"
+        });
+        return respuesta;
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export const editarServicio = async (servicio, id)=>{
+    try{
+        const respuesta = await fetch(URL_SERVICIOS+'/'+id,{
+            method: "PUT",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(servicio)
+        });
+        return respuesta;
+    }catch (error){
+        console.log(error)
+    }
+}
