f9ab66cea76f41e51b33b633caa2c993c2b92736
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-29 19:14:33 -0300

    utilizando el custom hook para mostrar los productos

--- src/components/views/Productos.jsx
+++ src/components/views/Productos.jsx
@@ -1,24 +1,35 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Spinner } from "react-bootstrap";
 import CardProducto from "./Producto/CardProducto";
-import { useState, useEffect } from "react";
+import { useFetchData } from "../hooks/useFetchData";
 
 const Productos = () => {
-  const [productos, setProductos] = useState([]);
+  const { data, isLoading, error } = useFetchData("productos");
+
+  const mostrarProductos = () => {
+    if (isLoading) {
+      return (
+        <div className="my-5">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      );
+    }
+
+    if (!isLoading && data?.length <= 0) {
+      return <h3 className="text-danger border p-3">{error}</h3>;
+    }
+
+    return data.map((producto) => (
+      <CardProducto producto={producto} key={producto.id} />
+    ));
+  };
+  
   return (
     <Container className="mt-3">
       <h1 className="text-center texto-productos fw-bold mt-1">
         Nuestros Productos
       </h1>
       <hr />
-      <Row>
-        {productos.length > 0 ? (
-          productos.map((producto) => (
-            <CardProducto producto={producto} key={producto.id} />
-          ))
-        ) : (
-          <h2>Hubo un error al cargar los productos</h2>
-        )}
-      </Row>
+      <Row>{mostrarProductos()}</Row>
     </Container>
   );
 };
