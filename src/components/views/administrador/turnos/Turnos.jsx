b56439754ea926e5fc4c23f085d32dfe084c9a13
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-31 00:49:20 -0300

    agregando maquetado de las cards turnos

--- src/components/views/administrador/turnos/Turnos.jsx
+++ src/components/views/administrador/turnos/Turnos.jsx
@@ -1,5 +1,166 @@
+import { Container, Row,Col,Card, Button } from "react-bootstrap";
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+
+
+
 const Turnos = () => {
-  return <></>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const Example  =()=>{
+    return (
+      <>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            I will not close if you click outside me. Don not even try to press
+            escape key.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary">Understood</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  return <>
+  <Container>
+    <div className="text-center"><Button>Crear Turno</Button></div>
+    <Row className="mt-5">
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <div className="d-flex justify-content-around">
+        <Card.Title>Card Title</Card.Title>
+        <Card.Title>
+          9:30 hs
+        </Card.Title>
+        </div>
+        <Card.Text>Aqui va la descripcion del turno Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem omnis distinctio molestiae quaerat totam dolorem vero alias nostrum sunt tempore.</Card.Text>
+       <div className="d-flex justify-content-around">
+       <Button variant="primary" onClick={handleShow}>
+       Editar Turno
+      </Button>
+        <Card.Link href="#" className="text-danger">Borrar turno</Card.Link>
+       </div>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+      <Col>
+      <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+      </Col>
+    </Row>
+  </Container>
+  <Example/>
+  </>;
 };
 
 export default Turnos;
