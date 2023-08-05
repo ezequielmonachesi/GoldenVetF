161d3c8caad7faa07d64851cbff8c51c60102453
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-04 22:05:22 -0300

    se agrego logica para no mostrar el paginado y mostrar un mensaje cuando no haya turnos

--- src/components/views/administrador/turnos/Turnos.jsx
+++ src/components/views/administrador/turnos/Turnos.jsx
@@ -1,11 +1,11 @@
-import React, {  useState } from 'react';
-import { Container, Row, Button, Spinner,Modal } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Button, Spinner, Modal } from 'react-bootstrap';
 import CardTurnosAdministrador from './CardTurnosAdministrador';
 import { useFetchData } from '../../../hooks/useFetchData';
 import FormularioTurno from '../../usuario/FormularioTurno';
 
 const Turnos = () => {
-  const { data, isLoading,refetchData } = useFetchData('turnos');
+  const { data, isLoading, refetchData } = useFetchData('turnos');
   const itemsPerPage = 12;
   const [currentPage, setCurrentPage] = useState(1);
   const [show, setShow] = useState(false);
@@ -41,37 +41,59 @@ const Turnos = () => {
             <Modal.Title>Crear Turno</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-           <FormularioTurno actualizarTurnos={actualizarTurnos}/>
+            <FormularioTurno actualizarTurnos={actualizarTurnos} />
           </Modal.Body>
         </Modal>
       </>
     );
   };
+
   return (
     <>
       <Container>
         <div className="d-flex justify-content-end">
           <Button onClick={handleShow}>Crear Turno</Button>
         </div>
-        <Row>
-          {isLoading ? (
-            <Spinner size="lg" variant="primary" />
-          ) : (
-            data.slice(startIndex, endIndex).map((turno) => (
-              <CardTurnosAdministrador turno={turno} key={turno.id} actualizarTurnos={actualizarTurnos}/>
-            ))
-          )}
-        </Row>
-        <div className="d-flex justify-content-center mt-3">
-          <Button variant="secondary" onClick={handlePrevPage} disabled={currentPage === 1} className='me-3'>
-            Anterior
-          </Button>
-          <Button variant="secondary" onClick={handleNextPage} disabled={currentPage === totalPages}>
-            Siguiente
-          </Button>
-        </div>
+        {data.length > 0 ? (
+          <Row>
+            {isLoading ? (
+              <Spinner size="lg" variant="primary" />
+            ) : (
+              data
+                .slice(startIndex, endIndex)
+                .map((turno) => (
+                  <CardTurnosAdministrador
+                    turno={turno}
+                    key={turno.id}
+                    actualizarTurnos={actualizarTurnos}
+                  />
+                ))
+            )}
+          </Row>
+        ) : (
+          <div className='text-center'><h3>No hay Turnos reservados</h3></div>
+        )}
+        {data.length > 0 && (
+          <div className="d-flex justify-content-center mt-3">
+            <Button
+              variant="secondary"
+              onClick={handlePrevPage}
+              disabled={currentPage === 1}
+              className="me-3"
+            >
+              Anterior
+            </Button>
+            <Button
+              variant="secondary"
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Siguiente
+            </Button>
+          </div>
+        )}
       </Container>
-      <ModalCrearTurno/>
+      <ModalCrearTurno />
     </>
   );
 };
