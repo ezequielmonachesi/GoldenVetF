f3938a79b51a8aa98981596039056ad67cbcb907
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-05 18:01:42 -0300

    agregando estilos al detalle del producto

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -14,32 +14,32 @@ const DetalleProducto = () => {
   }, []);
 
   return (
-    <Container>
-      <Row className="justify-content-around text-center text-md-start border shadow mt-5 pb-4 pb-md-0 rounded-3 mx-2">
+    <Container className="bg-light mb-2 mb-md-0 mb-lg-2">
+      <Row className="justify-content-center text-center text-md-start border shadow mt-2 pb-4 pb-md-0 rounded-3 mx-2">
         <Col sm={6}>
           <div>
-            <Image src={producto.imagen} fluid></Image>
+            <Image src={producto.imagen} className="w-75" fluid></Image>
           </div>
         </Col>
-        <Col sm={4} className="py-5 py-md-3 py-lg-5">
+        <Col sm={4} className="py-2 py-md-1 py-lg-5 ">
           <div className="text-end">
             <Button variant="outline-danger border-0">
               <Heart className="fs-5"></Heart>
             </Button>
           </div>
-          <h5 className="mt-3">{producto.nombreProducto}</h5>
+          <h5 className="mt-1 border text-danger">{producto.nombreProducto}</h5>
           <h3 className=" mt-3">$ {producto.precio}</h3>
           <Badge bg="danger" className="px-2 py-2 mt-3">
             1 Cuota Sin Interés
           </Badge>
-          <p className=" mt-3">Stock: {producto.stock}</p>
+          <p className="mt-3 fw-bold">Stock: {producto.stock}</p>
         </Col>
         <Col sm={12} className=" px-4 p-md-5">
-          <h6 className="text-start">Detalle del producto</h6>
-          <p className="small mt-3 text-start">
+          <h4 className="text-start">Detalle del producto</h4>
+          <p className="small mt-1 text-start">
             {mostrarMas
-              ? producto.descripcion
-              : `${producto.descripcion.substring(0, 250)}...`}
+              ? producto.descripcion.substring(0,100)
+              : `${producto.descripcion}...`}
           </p>
           <div className="text-end">
             <button
