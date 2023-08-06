4a573444e6f7f130433b312a37b09095feb88f3c
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-06 10:25:37 -0300

    agregando la lista de comentarios

--- src/components/views/administrador/comentarios/ListaComentarios.jsx
+++ src/components/views/administrador/comentarios/ListaComentarios.jsx
@@ -9,29 +9,6 @@ const L = ({ comentario, actualizarComentarios }) => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const eliminarComentario = () => {
-    Swal.fire({
-      title: '¿Estás seguro?',
-      text: 'Esta acción eliminará el comentario y no se puede deshacer.',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, eliminar',
-      cancelButtonText: 'Cancelar',
-      reverseButtons: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        borrarComentario(comentario.id).then((respuesta) => {
-          if (respuesta && respuesta.status === 200) {
-            Swal.fire('Comentario eliminado', 'El comentario fue eliminado correctamente', 'success');
-            actualizarComentarios();
-          } else {
-            Swal.fire('Error', 'El comentario no pudo ser eliminado', 'error');
-          }
-        });
-      }
-    });
-  };
-
   const ModalEditarComentario = ({ comentario }) => {
     return (
       <>
@@ -64,10 +41,10 @@ const L = ({ comentario, actualizarComentarios }) => {
                 <td>{comentario.creado}</td>
                 <td>
                   <div className="d-flex justify-content-around">
-                    <Button variant="primary" onClick={handleShow}>
+                    <Button variant="warning" onClick={handleShow}>
                       Editar Comentario
                     </Button>
-                    <Button variant="danger" onClick={eliminarComentario}>
+                    <Button variant="danger">
                       Borrar Comentario
                     </Button>
                   </div>
