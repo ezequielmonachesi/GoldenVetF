1e646787339e0f035584d31754826fb69973ae49
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-06 10:07:57 -0300

    se convierte a tabla administrador turnos

--- src/components/views/administrador/turnos/Turnos.jsx
+++ src/components/views/administrador/turnos/Turnos.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Container, Row, Button, Spinner, Modal } from 'react-bootstrap';
+import { Container, Table, Button, Spinner, Modal } from 'react-bootstrap';
 import CardTurnosAdministrador from './CardTurnosAdministrador';
 import { useFetchData } from '../../../hooks/useFetchData';
 import FormularioTurno from '../../usuario/FormularioTurno';
 
 const Turnos = () => {
   const { data, isLoading, refetchData } = useFetchData('turnos');
-  const itemsPerPage = 12;
+  const itemsPerPage = 8;
   const [currentPage, setCurrentPage] = useState(1);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -50,29 +50,44 @@ const Turnos = () => {
 
   return (
     <>
-      <Container>
-        <div className="d-flex justify-content-end">
-          <Button onClick={handleShow}>Crear Turno</Button>
+      <Container >
+        <div className="d-flex justify-content-end mb-4">
+          <Button variant='success' onClick={handleShow}>Crear Turno</Button>
         </div>
         {data.length > 0 ? (
-          <Row>
-            {isLoading ? (
-              <Spinner size="lg" variant="primary" />
-            ) : (
-              data
-                .slice(startIndex, endIndex)
-                .sort((a, b) => new Date(a.fechaYHora) - new Date(b.fechaYHora))
-                .map((turno) => (
-                  <CardTurnosAdministrador
-                    turno={turno}
-                    key={turno.id}
-                    actualizarTurnos={actualizarTurnos}
-                  />
-                ))
-            )}
-          </Row>
+          <Table responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>Paciente</th>
+                <th>Fecha y Hora</th>
+                <th>Veterinario</th>
+                <th>Detalle de la Visita</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {isLoading ? (
+                <div className='d-flex justify-content-center'>
+                    <Spinner animation="border" variant="primary" />
+                </div>
+              ) : (
+                data
+                  .slice(startIndex, endIndex)
+                  .sort((a, b) => new Date(a.fechaYHora) - new Date(b.fechaYHora))
+                  .map((turno) => (
+                    <CardTurnosAdministrador
+                      turno={turno}
+                      key={turno.id}
+                      actualizarTurnos={actualizarTurnos}
+                    />
+                  ))
+              )}
+            </tbody>
+          </Table>
         ) : (
-          <div className='text-center'><h3>No hay Turnos reservados</h3></div>
+          <div className="text-center">
+            <h3>No hay Turnos reservados</h3>
+          </div>
         )}
         {data.length > 0 && (
           <div className="d-flex justify-content-center mt-3">
@@ -96,6 +111,7 @@ const Turnos = () => {
       </Container>
       <ModalCrearTurno />
     </>
+
   );
 };
 
