a17dcf1e6de3b297a16d76edba3ae9339cc38222
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-08-07 02:30:45 -0300

    footer terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,10 +10,10 @@ import Error404 from "./components/views/error404/Error404";
 import Servicios from "./components/views/Servicios/Servicios";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
-import Productos from './components/views/Productos'
+import Productos from "./components/views/Productos";
 import AcercaDeNosotros from "./components/views/AcercaDeNosotros";
 import DetalleServicios from "./components/views/servicios/DetalleServicios";
-import DetalleProducto from './components/views/DetalleProducto'
+import DetalleProducto from "./components/views/DetalleProducto";
 import Planes from "./components/shared/Inicio/Planes";
 
 import Administrador from "./components/views/administrador/Administrador";
@@ -25,41 +24,60 @@ import PlanesVista from "./components/views/PlanesVista";
 import Contacto from "./components/views/contacto/Contacto";
 
 function App() {
-  const usuario = JSON.parse(sessionStorage.getItem('usuario')) || {};
+  const usuario = JSON.parse(sessionStorage.getItem("usuario")) || {};
   const [usuarioLogueado, setUsuarioLogueado] = useState(usuario);
 
   useEffect(() => {
-    sessionStorage.setItem('usuario', JSON.stringify(usuarioLogueado));
+    sessionStorage.setItem("usuario", JSON.stringify(usuarioLogueado));
   }, [usuarioLogueado]);
 
   return (
     <>
-
       <BrowserRouter>
-        <Menu usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado} />
-        <section className='mainSection'>
-        <Routes>          
-          <Route exact path="/" element={<Inicio></Inicio> }></Route>
-          <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}></Route>
-          <Route exact path="/registro" element={<Registro/>}></Route>
-          <Route exact path="/servicios" element={<Servicios/>}></Route>
-          <Route exact path="/servicios/:id" element={<DetalleServicios/>}></Route>
-          <Route exact path="/planes" element={<PlanesVista/>}></Route>
-          <Route exact path="/productos" element={<Productos/>}></Route>
-          <Route exact path="/productos/:id" element={<DetalleProducto/>}></Route>
-          <Route exact path="/planes" element={<Planes/>}></Route>
-          <Route exact path="/planes/:id" element={<DetallePlan/>}></Route>
-          <Route path="/admin/*" element={<Administrador />} />
-          <Route path="/usuario" element={<Usuario usuarioLogueado={usuarioLogueado} />} />
-          <Route path="*" element={<Error404></Error404>}></Route>
-          <Route path="/sobre-nosotros" element={<AcercaDeNosotros></AcercaDeNosotros>}></Route>
-          <Route path="/contacto" element={<Contacto></Contacto>}></Route>
-        </Routes>
-          </section>
-        <Footer/>
-
+        <Menu
+          usuarioLogueado={usuarioLogueado}
+          setUsuarioLogueado={setUsuarioLogueado}
+        />
+        <section className="mainSection">
+          <Routes>
+            <Route exact path="/" element={<Inicio></Inicio>}></Route>
+            <Route
+              exact
+              path="/login"
+              element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>}
+            ></Route>
+            <Route exact path="/registro" element={<Registro />}></Route>
+            <Route exact path="/servicios" element={<Servicios />}></Route>
+            <Route
+              exact
+              path="/servicios/:id"
+              element={<DetalleServicios />}
+            ></Route>
+            <Route exact path="/planes" element={<PlanesVista />}></Route>
+            <Route exact path="/productos" element={<Productos />}></Route>
+            <Route
+              exact
+              path="/productos/:id"
+              element={<DetalleProducto />}
+            ></Route>
+            <Route exact path="/planes" element={<Planes />}></Route>
+            <Route exact path="/planes/:id" element={<DetallePlan />}></Route>
+            <Route path="/admin/*" element={<Administrador />} />
+            <Route
+              path="/usuario"
+              element={<Usuario usuarioLogueado={usuarioLogueado} />}
+            />
+            <Route path="*" element={<Error404></Error404>}></Route>
+            <Route
+              path="/sobre-nosotros"
+              element={<AcercaDeNosotros></AcercaDeNosotros>}
+            ></Route>
+            <Route path="/contacto" element={<Contacto></Contacto>}></Route>
+            <Route path="*" element={<Error404></Error404>}></Route>
+          </Routes>
+        </section>
+        <Footer />
       </BrowserRouter>
-
     </>
   );
 }
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,14 +1,15 @@
-
 import {
   Facebook,
   GeoAltFill,
   Instagram,
   TelephoneFill,
   Twitter,
+  X,
 } from "react-bootstrap-icons";
 import { Link, useLocation } from "react-router-dom";
 import "../../App.css";
-
+import { Image, Col, Row, Container } from "react-bootstrap";
+import Logo from "../../assets/logo.png";
 
 const Footer = ({ path }) => {
   const { pathname } = useLocation();
@@ -16,10 +17,97 @@ const Footer = ({ path }) => {
 
   return (
     <>
-      <footer className="bg-footer text-light text-center p-5">
+      <footer className="bg-footer text-light text-center py-5">
+        <Container>
+          <Row className="gy-4">
+            <Col md={3}>
+              <div className="hoverEffect">
+                <Link to={"/"}>
+                  <Image src={Logo}></Image>
+                </Link>
+              </div>
+            </Col>
+            <Col md={3}>
+              <h5>Nosotros</h5>
+              <div className="d-flex flex-column">
+                <Link to={"/sobre-nosotros"} className="text-white hoverEffect">
+                  Sobre Nosotros
+                </Link>
+                <Link to={"/contacto"} className="text-white hoverEffect">
+                  Contacto
+                </Link>
+              </div>
+              <ul className="list-unstyled m-0">
+                <li className="hoverEffect text-white"></li>
+                <li></li>
+              </ul>
+            </Col>
+            <Col md={3}>
+              <h5>¡Seguinos!</h5>
+              <div className="d-flex flex-wrap justify-content-center">
+                <Link to={"pagina-no-encontrada"}>
+                  <div className="d-flex align-items-center text-white hoverEffect mx-1">
+                    <Instagram className="fs-6 mx-2" />
+                    <p className="m-0">Instagram</p>
+                  </div>
+                </Link>
+                <Link to={"pagina-no-encontrada"}>
+                  <div className="d-flex align-items-center text-white hoverEffect mx-1">
+                    <Facebook className="fs-6 mx-2" />
+                    <p className="m-0">Facebook</p>
+                  </div>
+                </Link>
+                <Link to={"pagina-no-encontrada"}>
+                  <div className="d-flex align-items-center text-white hoverEffect mx-1">
+                    <Twitter className="fs-6 mx-2" />
+                    <p className="m-0">
+                      Twitter - <span className="italic">X</span>
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            </Col>
+            <Col md={3}>
+              <h5>Ubicación</h5>
+              <a href="https://goo.gl/maps/Zfzw6vLcxR2r1QPN7" target="_blank">
+                <p className="text-md-start text-white hoverEffect">
+                  Gral. Paz 576, T4000 San Miguel de Tucumán, Tucumán
+                </p>
+              </a>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14240.408879082126!2d-65.2072018!3d-26.8367009!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1691380442500!5m2!1ses-419!2sar"
+                width="150"
+                height="100"
+                className="rounded-2 border-secondary"
+              ></iframe>
+            </Col>
+            <Col>
+              <hr />
+              <Row className="justify-content-center">
+                <Col md={4}>
+                  <p className="mb-0 mx-2 small">&copy; GoldenVet</p>
+                </Col>
+                <Col md={4}>
+                  <p className="mb-0 mx-2 small">
+                    Todos los derechos reservados 2023
+                  </p>
+                </Col>
+                <Col md={4}>
+                  <Link to={"pagina-no-encontrada"}>
+                    <p className="mb-0 mx-2 text-white small hoverEffect">
+                      Términos y condiciones
+                    </p>
+                  </Link>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+      {/* <footer className="bg-footer text-light text-center p-5">
         <article className="row w-100">
-          <section className="col-md-4">
-            <h3>Nuestas redes</h3>
+        <section className="col-md-4">
+        <h3>Nuestas redes</h3>
             <p>
               Instagram <Instagram className="text-black fs-4" />
             </p>
@@ -64,7 +152,7 @@ const Footer = ({ path }) => {
             </p>
           </section>
         </article>
-      </footer>
+      </footer> */}
     </>
   );
 };
