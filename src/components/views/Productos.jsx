1027e5f1beac0335efb02ff7239a7e95147c3aca
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-07 13:40:15 -0300

    condicional para mostrar un mensaje si no funciona el backend de los productos

--- src/components/views/Productos.jsx
+++ src/components/views/Productos.jsx
@@ -5,34 +5,28 @@ import { useFetchData } from "../hooks/useFetchData";
 const Productos = () => {
   const { data, isLoading } = useFetchData("productos");
 
-  const mostrarProductos = () => {
-    if (isLoading) {
-      return (
-        <div className="my-5 text-center">
-          <Spinner animation="border" variant="primary" />
-        </div>
-      );
-    }
-
-    if (!isLoading && data?.length <= 0) {
-      return (
-        <h3 className="text-danger p-3">
-          Hubo un error al cargar los productos
-        </h3>
-      );
-    }
-
-   return data.map(producto=><CardProducto producto={producto} key={producto.id}/>)
-  };
-
   return (
     <Container className="my-5">
       <div className="bg-light rounded shadow p-sm-2 p-md-5">
-                <h1 className="text-center">Nuestros productos</h1>
-                <hr />
-                <p className="text-center plan-description">Desde alimentos de alta calidad hasta juguetes entretenidos, pasando por productos de higiene y cuidado, GoldenVet tiene todo lo que necesitas para mantener a tu querido amigo feliz y saludable en cada etapa de su vida.</p>
-            </div>
-      <Row className="d-flex justify-content-center my-5">{mostrarProductos()}</Row>
+        <h1 className="text-center">Nuestros productos</h1>
+        <hr />
+        <p className="text-center plan-description">
+          Desde alimentos de alta calidad hasta juguetes entretenidos, pasando
+          por productos de higiene y cuidado, GoldenVet tiene todo lo que
+          necesitas para mantener a tu querido amigo feliz y saludable en cada
+          etapa de su vida.
+        </p>
+      </div>
+      <Row className="d-flex justify-content-center my-5">
+        {isLoading ?<div className="my-5 text-center">
+          <Spinner animation="border" variant="primary" />
+        </div> :<h3 className="text-danger p-3">
+          Hubo un error al cargar los productos. Por favor, inténtalo de nuevo más tarde.
+        </h3>}
+        {data?.length > 0 && data.map((producto) => (
+      <CardProducto producto={producto} key={producto.id} />
+    ))}
+      </Row>
     </Container>
   );
 };
