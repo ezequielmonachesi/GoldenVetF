d9d5c1a7f8d0d5d0b239b4ad0e08ad9e01181cdb
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-07 09:33:50 -0300

    se quita if innecesario en componente usuario

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const RutasProtegidas = ({children}) => {
    const usuarioLogueado = JSON.parse(sessionStorage.getItem('usuario')) || null;
-   if(!usuarioLogueado.rol){
+   if(!usuarioLogueado.nombreUsuario){
     return <Navigate to={'/login'}></Navigate>
    }else{
     return children;
