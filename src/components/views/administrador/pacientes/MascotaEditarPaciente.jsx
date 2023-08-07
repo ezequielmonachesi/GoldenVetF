ed492f1b7366522d7da8b5912082c52da6f241f0
Silvio Ruiz <silviojrck@gmail.com>
2023-08-07 17:50:09 -0300

    Ahora se puede agregar historial medico a las mascotas

--- src/components/views/administrador/pacientes/MascotaEditarPaciente.jsx
+++ src/components/views/administrador/pacientes/MascotaEditarPaciente.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 import { Button, Card, Col, Modal, Row } from "react-bootstrap";
 import FormularioEditarMascota from "./FormularioEditarMascota";
+import { editarPaciente } from "../../../helpers/queriesPacientes";
+import Swal from "sweetalert2";
+import FormularioAgregarHistoriaMedica from "./FormularioAgregarHistoriaMedica";
 
 const MascotaEditarPaciente = ({ paciente, mascota }) => {
     const [modalShowEditarMascota, setModalShowEditarMascota] = useState(false);
+    const [modalShowAgregarHistoriaMedica, setModalShowAgregarHistoriaMedica] = useState(false);
+
+    function eliminarMascota(mascotaAEliminar) {
+        const nuevasMascotas = paciente.mascotas.filter((mascotaActual) => mascotaActual.nombre !== mascotaAEliminar.nombre);
+        paciente.mascotas = nuevasMascotas;
+        editarPaciente(paciente, paciente.id).then((respuesta) => {
+            if (respuesta && respuesta.status === 200) {
+                Swal.fire('Mascota eliminada', `La mascota ${mascotaAEliminar.nombre} fue eliminada correctamente`, 'success');
+            } else {
+                Swal.fire('Ocurrió un error', `La mascota ${mascotaAEliminar.nombre} no pudo ser eliminada, intente en unos minutos`, 'error');
+            }
+        })
+    }
 
     function ModalAgregarMascota(props) {
         return (
@@ -24,7 +40,28 @@ const MascotaEditarPaciente = ({ paciente, mascota }) => {
             </Modal.Footer>
           </Modal>
         );
-      }
+    }
+
+    function ModalAgregarHistoriaMedica(props) {
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>Agregar historia medica</Modal.Header>
+            <Modal.Body>
+              <FormularioAgregarHistoriaMedica paciente={paciente} mascota={mascota}></FormularioAgregarHistoriaMedica>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="danger" onClick={props.onHide}>
+                Salir
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        );
+    }
 
     return (
         <>
@@ -45,10 +82,12 @@ const MascotaEditarPaciente = ({ paciente, mascota }) => {
                         </Col>
                     </Row>
                     <Button onClick={() => setModalShowEditarMascota(true)} className="m-2" variant="success">Editar</Button>
-                    <Button variant="danger">borrar</Button>
+                    <Button onClick={() => setModalShowAgregarHistoriaMedica(true)} variant="danger">Agregar historia</Button>
+                    <Button onClick={() => eliminarMascota(mascota)} className="m-2" variant="danger">borrar</Button>
                 </Card.Body>
             </Card>
             <ModalAgregarMascota show={modalShowEditarMascota} onHide={() => setModalShowEditarMascota(false)} />
+            <ModalAgregarHistoriaMedica show={modalShowAgregarHistoriaMedica} onHide={() => setModalShowAgregarHistoriaMedica(false)} />
         </>
     );
 };
