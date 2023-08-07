3fde7e865cf627b9bf682a1e5fd98d56ebb12503
Diego Mercado <diego.mercado1997@hotmail.com>
2023-08-07 00:14:04 -0300

    creando componente RutasProtegidas

---
+++ src/components/routes/RutasProtegidas.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+const RutasProtegidas = ({children}) => {
+   const usuarioLogueado = JSON.parse(sessionStorage.getItem('usuario')) || null;
+   if(!usuarioLogueado){
+    return <Navigate to={'/login'}></Navigate>
+   }else{
+    return children;
+   }
+};
+
+export default RutasProtegidas;
