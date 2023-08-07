76b1075436aa1551184a484558c0d73e4103c736
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-08-07 20:09:34 -0300

    slider funcionando

--- src/components/views/BannerPublicidad.jsx
+++ src/components/views/BannerPublicidad.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import { Container, Carousel, Image } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import images from "../../exports/images";
 import { motion } from "framer-motion";
 
 const BannerPublicidad = () => {
   return (
-    <>
+    <Container fluid>
+      <h4>Marcas con las que trabajamos</h4>
       <motion.div className="slider-container">
-        <motion.div className="slider" drag="x" dragConstraints={{ right: 0, left: 0 }}>
+        <motion.div
+          className="slider"
+          drag="x"
+          dragConstraints={{ right: 0, left: 0 }}
+        >
           {images.map((image, key) => (
             <motion.div className="item">
-              <img src={image} id={key} className="w-100"></img>
+              <img
+                src={image}
+                id={key}
+                className="w-100 bg-white rounded-3"
+              ></img>
             </motion.div>
           ))}
         </motion.div>
       </motion.div>
-    </>
+    </Container>
   );
 };
 
