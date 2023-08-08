ed266ea251647fd20fa383fa255df92e520a069a
Silvio Ruiz <silviojrck@gmail.com>
2023-08-08 01:29:30 -0300

    Agrega detalle de mascota a los usuarios

--- src/components/views/usuario/CardMascotaUsuario.jsx
+++ src/components/views/usuario/CardMascotaUsuario.jsx
@@ -1,20 +1,49 @@
-import { Button, Card, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Card, Col, Modal } from 'react-bootstrap';
+import DetalleMascota from './DetalleMascota';
 
-const CardMascotaUsuario = ({nombre, especie, raza, historialMedico, imagen}) => {
+const CardMascotaUsuario = ({ mascota }) => {
+
+    const [modalShowDetalleMascota, setModalShowDetalleMascota] = useState(false);
+
+    function ModalDetalleMascota(props) {
+        return (
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>Detalle mascota</Modal.Header>
+                <Modal.Body>
+                    <DetalleMascota mascota={mascota}></DetalleMascota>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={props.onHide}>
+                        Salir
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
     return (
-        <Col sm={6}>
-            <Card className='my-3 h-100'>
-                <Card.Img className='img-fluid' variant="top" src={imagen} />
-                <Card.Body>
-                    <Card.Title>{nombre}</Card.Title>
+        <>
+            <Col sm={6}>
+                <Card className='my-3 h-100'>
+                    <Card.Img className='img-fluid' variant="top" src={mascota.imagen} />
+                    <Card.Body>
+                        <Card.Title>{mascota.nombre}</Card.Title>
                         <ul>
-                            <li>Especie: {especie}</li>
-                            <li>Raza: {raza}</li>
+                            <li>Especie: {mascota.especie}</li>
+                            <li>Raza: {mascota.raza}</li>
                         </ul>
-                    <Button variant="success">Ver mas</Button>
-                </Card.Body>
-            </Card>
-        </Col>
+                        <Button onClick={() => setModalShowDetalleMascota(true)} variant="success">Ver mas</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <ModalDetalleMascota show={modalShowDetalleMascota} onHide={() => setModalShowDetalleMascota(false)} />
+        </>
     );
 };
 
