b20decec22dd9731ed84da8de51a0dc8ad037c57
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-08-08 02:50:03 -0300

    se termina de corregir estilos y se agrega bienvenida

---
+++ src/components/shared/Inicio/BienvenidaInicio.jsx
@@ -0,0 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const BienvenidaInicio = () => {
+  return (
+    <Container fluid className=" bg-light p-3 p-md-5 shadow">
+      <h1 className="text-center display-2 py-2">¡Bienvenido a Golden Vet!</h1>
+      <p className="plan-description text-center py-2">
+        Cuidamos de tus mascotas con amor y experiencia. Nuestro equipo está
+        listo para brindarles la mejor atención y cariño. Ven y descubre cómo
+        hacemos que las colitas meneen de felicidad en Golden Vet. Equipo de
+        Golden Vet
+      </p>
+    </Container>
+  );
+};
+
+export default BienvenidaInicio;
